0789f571d205bd037bdc437458fdc52f1b6759e2
emineyerdelen <emineyerdelen20@gmail.com>
2025-02-24 12:53:38 +0300

    Ders 8 sonu

---
+++ Infrastructure/MovieApi.Persistence/Migrations/20250224083051_mig2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedeYear",
+                table: "Movies",
+                newName: "CreatedYear");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedYear",
+                table: "Movies",
+                newName: "CreatedeYear");
+        }
+    }
+}
